cfad94151bcb0ceb0a267a65adf590ba05130d19
ParaHuang <paradox041@gmail.com>
2024-03-27 09:46:46 +0800

    singleton instance

--- BhuvanaDemo/src/Main.java
+++ BhuvanaDemo/src/Main.java
@@ -2,16 +2,13 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //create an object/instance of Account
+        Account account = new Account();// new Xxx() is using the constructor
+        account.setOwner("Jack");
+        account.setBalance(2000);
+        account.setFrameXxx(3.5);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Account account2 = new Account("Alex",5000,4.5);
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
